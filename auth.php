69a13ccfbb5826e9948f2f7e00b159fa319f22f2
MrBasenji <34125370+MrBasenji@users.noreply.github.com>
2019-02-19 21:39:23 +0100

    Update auth.php

--- auth.php
+++ auth.php
@@ -1,10 +1,11 @@
 <?php
+	require_once 'vendor/autoload.php';
 	use ReallySimpleJWT\Token;
 	
-	$secret = getenv('PHP_SECRET');
 	
 	function createJWT($user)
 	{
+		$secret = getenv('PHP_SECRET');
 		$userid = $user['id'];
 		$expiration = time() + 3600;
 		$issuer = 'localhost';		
@@ -15,6 +16,7 @@
 	
 	function validateJWT(string $token): bool
 	{
+		$secret = getenv('PHP_SECRET');
 		return Token::validate($token, $secret);
 	}
 ?>
